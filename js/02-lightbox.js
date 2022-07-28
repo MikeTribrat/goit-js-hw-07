87e09f103fa958050104296207c32b9c0f9b75a1
MikeTribrat <mike.lapp.ua@gmail.com>
2022-07-28 18:05:50 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+//Создание и рендер разметки по массиву данных galleryItems
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => { 
+    return `<div class="gallery__list">
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}", alt="${description}",/>
+    </a>
+</div>`;
+}).join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+//Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery
+
+const lightboxGallery = new SimpleLightbox('.gallery a', {
+    captions: 'true',
+    captionsData: 'alt',
+    captionDelay: 250,
+    
+});
+
